78496a431eb8482614c270ee37123e80559deebc
neojarvis <techadmin@iamneo.ai>
2024-01-24 06:19:12 +0000

    Auto-save-commit-1/24/2024--11:49:05-AM

--- sh.sh
+++ sh.sh
@@ -1,5 +1,5 @@
 cp /home/coder/project/workspace/pytest/tests.py /home/coder/project/workspace/Day1Q2/Q2/tests.py44
 echo "line one"
-cd /home/coder/project/workspace/Day1Q2
+cd /home/coder/project/workspace/Day1Q2/Q2
 echo "line one"
 python3 manage.py test -v 0
